5dd8862896c76c9fc3f726fdd16bb37dcd8790ec
wahoob <majed.eddin.wahoub@gmail.com>
2024-09-02 01:32:12 +0300

    fixing error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.css";
-import { ToDoProvider } from "./context/todoContext.jsx";
+import { ToDoProvider } from "./context/toDoContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
